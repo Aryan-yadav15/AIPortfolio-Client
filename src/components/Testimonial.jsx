698e57d9523359e20082c58501dd754025e3fabb
Aryan-yadav15 <a2003yadav@gmail.com>
2024-06-21 13:13:19 +0530

    testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import InfiniteMovingCards from "./sections/InfiniteMovingCards";
 
 
-export function  Testimonial() {
+export function Testimonial() {
   return (
     <div className="h-[30rem] rounded-md flex flex-col antialiased bg-white dark:bg-customDarkGray dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
@@ -20,30 +20,30 @@ const testimonials = [
   {
     quote:
       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+    name: "Priya K.",
+    title: "Game-changing AI assistant!",
   },
   {
     quote:
       "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
+    name: "Rahul M.",
+    title: "Revolutionizing Smart Homes",
   },
   {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
+    quote: "ZORA listens, learns, and adapts to my preferences effortlessly. It's the most personalized AI assistant I've ever used. My productivity has skyrocketed!",
+    name: "Neha S.",
+    title: "Unmatched Personalization",
   },
   {
     quote:
       "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
+    name: " Amit R.",
+    title: "Efficiency at its best",
   },
   {
     quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
+      "ZOSS's intelligent sensors and ZORA's real-time data updates provide an unparalleled smart home experience. It's like living in the future. Excellent work by ZebiOps!",
+    name: "Sunita P.",
+    title: "Innovative and Smart",
   },
 ];
